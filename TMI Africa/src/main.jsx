83aed4b19614fd2cbcb3f260c66a27f0049bf5f7
FredMulenga123 <zerb@zambia.co.zm>
2026-05-28 23:35:11 +0200

    Update main.jsx

--- TMI Africa/src/main.jsx
+++ TMI Africa/src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function TerraNovaAfrica() {
   return (
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-6xl font-black text-cyan-400">
-        TerraNova Working
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-cyan-400">
+        TerraNova Africa Working
       </h1>
-    </main>
+    </div>
   );
 }
