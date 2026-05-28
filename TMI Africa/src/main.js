62b959a9d891fd00671ca33f317dd677009544ac
FredMulenga123 <zerb@zambia.co.zm>
2026-05-28 22:43:17 +0200

    Create main.js

---
+++ TMI Africa/src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TerraNovaAfrica() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl font-black text-cyan-400">
+        TerraNova Working
+      </h1>
+    </main>
+  );
+}
