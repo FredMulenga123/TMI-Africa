c34ac0815f48df2fc665defddae09c0d75d4843b
FredMulenga123 <zerb@zambia.co.zm>
2026-05-28 23:47:23 +0200

    Create main.js

---
+++ TMI Africa/src/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TerraNovaAfrica from "./main.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TerraNovaAfrica />
+  </React.StrictMode>
+);
