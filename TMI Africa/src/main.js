e9b9dd796f01d8a5b5b32f2fcad8f4396a5d2dd5
FredMulenga123 <zerb@zambia.co.zm>
2026-05-28 22:51:34 +0200

    Update main.js

--- TMI Africa/src/main.js
+++ TMI Africa/src/main.js
@@ -1,11 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TerraNovaAfrica from "./main.jsx";
 
-export default function TerraNovaAfrica() {
-  return (
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-6xl font-black text-cyan-400">
-        TerraNova Working
-      </h1>
-    </main>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TerraNovaAfrica />
+  </React.StrictMode>
+);
